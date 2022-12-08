90d474bc84feaa4386a295ab7e013bb5145b942d
Adedeji1 <afolabiadedeji10@yahoo.com>
2022-12-08 15:03:03 -0800

    5-print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,11 @@
 
 /**
  * main - Entry Point
- * 
- * Return: Always 0 (success)
+ *
+ * Return: 0 (Success)
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-}	
+}
